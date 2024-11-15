230580439849a8ec9d26963ea31349ca9418884d
pppi21 <leorodarte07@gmail.com>
2024-11-15 09:56:43 -0800

    11/15
    New jframe for browser management unfinished

--- src/main/java/com/pppi/selenium_test/Launcher.java
+++ src/main/java/com/pppi/selenium_test/Launcher.java
@@ -17,6 +17,7 @@ public class Launcher {
 
    
         JButton startButton = new JButton("Start Browser");
+        JButton allBrowsers = new JButton("Manage Browsers");
         JLabel browserCountLabel = new JLabel("Open Browsers: 0");
         JButton browserInfo = new JButton("Current Proxy:");
         JTextField proxyField = new JTextField(20);
@@ -47,14 +48,37 @@ public class Launcher {
             }
         });
 
+        allBrowsers.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		JFrame browserFrame = new JFrame("Browser Manager");
+        		JPanel browserPanel = new JPanel();
+        		browserPanel.setLayout(new BoxLayout(browserPanel, BoxLayout.Y_AXIS));
+        	
+        		JScrollPane browserScroll = new JScrollPane(browserPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        		
+        		for(int i = 0; i < 20; i++) {
+        			JPanel row = newRow(browserPanel);
+        		browserPanel.add(row);
+        		for(int j = 0; j < 3; j++) {
+        		row.add(newBox(row));
+        		}
+        		}
+        		browserFrame.pack();
+        		browserFrame.add(browserScroll);
+        		browserFrame.setVisible(true);
+        	}
+        });
+        
+        
 
        
         panel.add(proxyLabel);
         panel.add(proxyField);
         panel.add(secProxyField);
         panel.add(startButton);
-        panel.add(switchButton);
-        panel.add(currProxy);
+        panel.add(allBrowsers);
+ //       panel.add(currProxy);
         panel.add(browserCountLabel);
         
         frame.add(panel);
@@ -64,6 +88,20 @@ public class Launcher {
         
             }
 	
+	public static JPanel newRow(JPanel e) {
+		JPanel newPanel = new JPanel();
+    	newPanel.setLayout(new BoxLayout(newPanel, BoxLayout.X_AXIS));
+    	
+    	return newPanel;
+    }
+	
+	public static JPanel newBox(JPanel e) {
+		JPanel newPanel = new JPanel();
+		newPanel.setLayout(new BoxLayout(newPanel, BoxLayout.Y_AXIS));
+		JLabel test = new JLabel("Open Browsers: 0");
+		newPanel.add(test);
+    	return newPanel;
+	}
 		
 
 }
